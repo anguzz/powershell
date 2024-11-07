1eb5d82e580aac22d3ae993adcebbd5e9579d35d
Angel Santoyo <santoyox714@gmail.com>
2024-11-07 10:11:06 -0800

    Update remove_local_users.ps1

--- remove_local_users.ps1
+++ remove_local_users.ps1
@@ -1,4 +1,23 @@
-$AccountsToKeep = @('Administrator', 'Admin', 'Public', 'Default') #can add more to keep here, if its domained account do domain.com\account_name
-Get-CimInstance -Class Win32_UserProfile | Where-Object { $_.LocalPath.split('\')[-1] -notin $profilesToKeep } | Remove-CimInstance
+
+#will delete users on a device over the network, has a list of accounts to keep
 
 
+$cred = Get-Credential
+$computer = Read-Host "Enter the computer name"
+$AccountsToKeep = @('Administrator', 'Admin', 'Public', 'Default', 'niagara', 'niagarawater.com\PDQInventory', 'pdqinventory', 'NetworkService', 'LocalService', 'systemprofile')
+
+
+Invoke-Command -ComputerName $computer -Credential $cred -ScriptBlock {
+    $UserProfiles = Get-CimInstance -Class Win32_UserProfile | Where-Object {
+        $_.LocalPath.Split('\')[-1] -notin $using:AccountsToKeep
+    }
+
+    foreach ($UserProfile in $UserProfiles) {
+        try {
+            Remove-CimInstance -InputObject $UserProfile -Confirm:$false
+            Write-Host "Removed profile for $($UserProfile.LocalPath.Split('\')[-1])"
+        } catch {
+            Write-Host "Failed to remove profile for $($UserProfile.LocalPath.Split('\')[-1]): $($_.Exception.Message)"
+        }
+    }
+}
