ac43bb2a84b1f7203422048f9d6f18fe3be551b0
Angel Santoyo <santoyox714@gmail.com>
2024-11-13 11:39:11 -0800

    Update get_processes.ps1

--- get_processes.ps1
+++ get_processes.ps1
@@ -1,19 +1,33 @@
-$hostName = Read-Host "Enter host name"
+#  script to check updates, services, processes, and scheduled jobs on a remote machine via device name 
 
-if (-not (Test-Connection -ComputerName $hostName -Count 1 -Quiet)) {
-    Write-Output "The host '$hostName' is offline."
-    exit
+$computerName = Read-Host "Please enter the computer name"
+$credentials = Get-Credential
+
+$cimSession = New-CimSession -ComputerName $computerName -Credential $credentials
+
+$updates = Get-CimInstance -ClassName Win32_QuickFixEngineering -CimSession $cimSession
+Write-Output "Installed Updates:"
+foreach ($update in $updates) {
+    Write-Output "    Hotfix ID: $($update.HotFixID), Description: $($update.Description), Installed On: $($update.InstalledOn)"
 }
 
-$credentials = Get-Credential
-$cimSession = New-CimSession -ComputerName $hostName -Credential $credentials
-$runningProcesses = Get-CimInstance -ClassName Win32_Process -CimSession $cimSession
+$services = Get-CimInstance -ClassName Win32_Service -CimSession $cimSession
+Write-Output "Services Info:"
+foreach ($service in $services) {
+    Write-Output "    Name: $($service.Name), Status: $($service.State), Start Mode: $($service.StartMode)"
+}
 
-Write-Output "List of all running processes on ${hostName}:"
-foreach ($process in $runningProcesses) {
-    Write-Output "$($process.ProcessId) - $($process.Name)"
+$processes = Get-CimInstance -ClassName Win32_Process -CimSession $cimSession
+Write-Output "Processes Info:"
+foreach ($process in $processes) {
+    Write-Output "    Process Name: $($process.Name), Process ID: $($process.ProcessId), Memory Usage: $($process.WorkingSetSize / 1MB) MB"
+}
+
+$jobs = Get-CimInstance -ClassName Win32_ScheduledJob -CimSession $cimSession
+Write-Output "Scheduled Jobs Info:"
+foreach ($job in $jobs) {
+    Write-Output "    Job ID: $($job.JobId), Command: $($job.Command), Next Run Time: $($job.NextRun)"
 }
 
 Remove-CimSession -CimSession $cimSession
-
 Write-Output "CIM session closed."
