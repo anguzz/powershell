81920202f239779217d3375181c10e504615adb4
Angel Santoyo <santoyox714@gmail.com>
2025-03-21 15:41:10 -0700

    Update uninstall.ps1

--- intune/pwExpireNotify/uninstall.ps1
+++ intune/pwExpireNotify/uninstall.ps1
@@ -1,15 +1,16 @@
-$taskName = "CheckUserPasswordPolicy"
-$destinationPath = ""
+$AccessTokenName = "GRAPH_PW_EXPIRE_TOKEN"
+$destinationPath = "C:\Program Files (x86)\pwExpireNotifyClient"
 $logFile = Join-Path $destinationPath "uninstallLog.txt"
 
-
+# removes the scheduled task
 try {
-    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
-    Add-Content -Path $logFile -Value "Scheduled task '$taskName' unregistered successfully."
+    Unregister-ScheduledTask -TaskName "CheckUserPasswordPolicy" -Confirm:$false
+    Add-Content -Path $logFile -Value "Scheduled task 'CheckUserPasswordPolicy' unregistered successfully."
 } catch {
     Add-Content -Path $logFile -Value "Failed to unregister scheduled task: $_"
 }
 
+# deletes the installation directory
 try {
     Remove-Item -Path $destinationPath -Recurse -Force
     Add-Content -Path $logFile -Value "Deleted folder '$destinationPath' and all contents."
@@ -17,25 +18,16 @@ try {
     Add-Content -Path $logFile -Value "Failed to delete folder: $_"
 }
 
-$taskName = "CheckUserPasswordPolicy"
-$destinationPath = ""
-$logFile = Join-Path $destinationPath "uninstallLog.txt"
+# removes the environment variable
+try {
+    [System.Environment]::SetEnvironmentVariable($AccessTokenName, $null, [System.EnvironmentVariableTarget]::Machine)
+    Add-Content -Path $logFile -Value "Removed environment variable '$AccessTokenName'."
+} catch {
+    Add-Content -Path $logFile -Value "Failed to remove environment variable '$AccessTokenName': $_"
+}
+
+# try to remove powershell modules for auth
 $modules = @("Microsoft.Graph.Authentication", "Microsoft.Graph.Users")
-
-try {
-    Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
-    Add-Content -Path $logFile -Value "Scheduled task '$taskName' unregistered successfully."
-} catch {
-    Add-Content -Path $logFile -Value "Failed to unregister scheduled task: $_"
-}
-
-try {
-    Remove-Item -Path $destinationPath -Recurse -Force
-    Add-Content -Path $logFile -Value "Deleted folder '$destinationPath' and all contents."
-} catch {
-    Add-Content -Path $logFile -Value "Failed to delete folder: $_"
-}
-
 foreach ($module in $modules) {
     try {
         Uninstall-Module -Name $module -AllVersions -Force
@@ -44,4 +36,3 @@ foreach ($module in $modules) {
         Add-Content -Path $logFile -Value "Failed to uninstall $module module: $_"
     }
 }
-
