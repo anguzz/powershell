dd85f472f24bb826a20ddf7c18d56c982ccf6cb8
Angel Santoyo <santoyox714@gmail.com>
2025-04-07 13:13:21 -0700

    Update getDevicesDeviceGroup.ps1
    handles pagination

--- Graph/getDevicesFromGroups/getDevicesDeviceGroup.ps1
+++ Graph/getDevicesFromGroups/getDevicesDeviceGroup.ps1
@@ -1,53 +1,50 @@
-#this script gets devices in a device group, without users. 
-
-
 Connect-MgGraph -Scopes "User.ReadWrite.All", "DeviceManagementManagedDevices.ReadWrite.All", "Directory.ReadWrite.All"
 
 $groupId = ""
 
-# fetch devices within the group
-$devicesUri = "https://graph.microsoft.com/v1.0/groups/$groupId/members`?`$select=id"
-$groupDevicesResponse = Invoke-MgGraphRequest -Uri $devicesUri -Method GET -OutputType PSObject
+$devicesUri = "https://graph.microsoft.com/v1.0/groups/$groupId/members`?`$select=id&`$top=999"
 
-Write-Output "`n-----------------------------------------------------------------`n"
-
-$groupDisplayNameURL = "https://graph.microsoft.com/v1.0/groups/$groupId`?`$select=displayName"
-$groupDisplayResponse = Invoke-MgGraphRequest -Uri $groupDisplayNameURL -Method GET -OutputType PSObject
-$groupDisplayName = $groupDisplayResponse.displayName
-
-Write-Output "Starting device report for " $groupDisplayName
-
-Write-Output "`n-----------------------------------------------------------------`n"
+$continue = $true
 $csvData = @()
 
-if ($null -eq $groupDevicesResponse.value -or $groupDevicesResponse.value.Count -eq 0) {
-    Write-Output "No devices found in the group or failed to fetch devices."
-    return
-} else {
-    foreach ($device in $groupDevicesResponse.value) {
-        $deviceID = $device.id
-        Write-Host "Device ID: $deviceID added"
+while ($continue) {
+    $groupDevicesResponse = Invoke-MgGraphRequest -Uri $devicesUri -Method GET -OutputType PSObject
 
+    if ($null -eq $groupDevicesResponse.value -or $groupDevicesResponse.value.Count -eq 0) {
+        Write-Output "No more devices found in the group or failed to fetch devices."
+        $continue = $false
+    } else {
+        foreach ($device in $groupDevicesResponse.value) {
+            $deviceID = $device.id
+            Write-Host "Device ID: $deviceID added"
 
-        try {
-            $selectString= "id,displayName,operatingSystem,operatingSystemVersion,manufacturer,model,serialNumber"#makes script query a bit faster by not grabbing whole response per object
-            $deviceDetailUri = "https://graph.microsoft.com/v1.0/devices/$deviceID`?`$select=$selectString"
-            $deviceResponse = Invoke-MgGraphRequest -Uri $deviceDetailUri -Method GET -OutputType PSObject
+            try {
+                $selectString= "id,displayName,operatingSystem,operatingSystemVersion,manufacturer,model,serialNumber"  
+                $deviceDetailUri = "https://graph.microsoft.com/v1.0/devices/$deviceID`?`$select=$selectString"
+                $deviceResponse = Invoke-MgGraphRequest -Uri $deviceDetailUri -Method GET -OutputType PSObject
 
-            $deviceObject = [PSCustomObject]@{
-                "Device ID" = $deviceResponse.id
-                "Device name" = $deviceResponse.displayName
-                "OS" = $deviceResponse.operatingSystem
-                "OS version" = $deviceResponse.OperatingSystemVersion
-                "Manufacturer" = $deviceResponse.manufacturer
-                "Model" = $deviceResponse.model
-                "Serial number" = $deviceResponse.serialNumber
-                "Primary user" = $deviceResponse.Primary
+                $deviceObject = [PSCustomObject]@{
+                    "Device ID" = $deviceResponse.id
+                    "Device name" = $deviceResponse.displayName
+                    "OS" = $deviceResponse.operatingSystem
+                    "OS version" = $deviceResponse.operatingSystemVersion
+                    "Manufacturer" = $deviceResponse.manufacturer
+                    "Model" = $deviceResponse.model
+                    "Serial number" = $deviceResponse.serialNumber
+                    "Primary user" = $deviceResponse.Primary
+                }
+                $csvData += $deviceObject
+
+            } catch {
+                Write-Output "Failed to fetch details for device ID $deviceID $($_.Exception.Message)"
             }
-            $csvData += $deviceObject
+        }
 
-        } catch {
-            Write-Output "Failed to fetch details for device ID $deviceID $($_.Exception.Message)"
+        # checks for next link to continue 
+        if ($groupDevicesResponse.'@odata.nextLink') {
+            $devicesUri = $groupDevicesResponse.'@odata.nextLink'
+        } else {
+            $continue = $false
         }
     }
 }
