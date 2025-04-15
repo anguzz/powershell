32b7353371d2cc4bc427e04cf58c786e6e5d0871
Angel Santoyo <santoyox714@gmail.com>
2025-04-15 11:42:20 -0700

    update install/uninstall
    remove requires line

--- intune/pwExpireNotify/install.ps1
+++ intune/pwExpireNotify/install.ps1
@@ -1,6 +1,5 @@
 
 
-#Requires -RunAsAdministrator
 
 
 param (
--- intune/pwExpireNotify/uninstall.ps1
+++ intune/pwExpireNotify/uninstall.ps1
@@ -1,6 +1,5 @@
 
 
-#Requires -RunAsAdministrator
 
 #--------------------------------------------------------------------------
 # Initialization & Configuration
